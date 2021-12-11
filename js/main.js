d2cdf929d93a42aacf2408210877de05c15f8e20
AlexLazo <alex.lazo1403@gmail.com>
2021-12-11 09:49:47 -0600

    main

--- js/main.js
+++ js/main.js
@@ -6,6 +6,32 @@ jQuery(document).ready(function($) {
         $("#tabs").tabs();
     });
 
+    //Botón de regresar arriba
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        // Deslizar hacia arriba
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // Script para deslizar arriba
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+        // Eliminar el desplazamiento suave en el clic del botón deslizante
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function() {
+        // Aplicando denuevo el desplazamiento suave en los elementos del menú
+        $('html').css("scrollBehavior", "smooth");
+    });
+
 
     // Animación de la página de carga
 
